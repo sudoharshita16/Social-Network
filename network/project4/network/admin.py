451b1e059ca53f4ecd46d9a7d33ee1627227aed4
vaishnav <vaishnav291999@gmail.com>
2022-06-03 22:15:48 +0530

    add comment model, add search option frontend in nav bar

--- network/project4/network/admin.py
+++ network/project4/network/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Post, UserProfile
+from .models import User, Post, UserProfile, Comments
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(Post)
+admin.site.register(Comments)
--- network/project4/network/models.py
+++ network/project4/network/models.py
@@ -59,6 +59,14 @@ class Post(models.Model):
         return f"{self.data} by {self.user}"
 
 
+class Comments(models.Model):
+    user = models.ForeignKey(User ,on_delete = models.CASCADE, related_name = "comments")
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = "comments")
+    comment = models.TextField(null = True)
+    time = models.DateTimeField(blank=True)
+    commentLikers = models.ManyToManyField(User, related_name = "liked_comments",blank=True)
 
+    def __str__(self):
+        return f"by {self.user}, content is- {self.comment}"
 
 
